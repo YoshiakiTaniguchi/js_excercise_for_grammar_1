68a28c1e892e06999029b90490f790314cb96173
YoshiakiTaniguchi <tanitani717@outlook.com>
2019-01-19 16:04:53 +0900

    second commit

--- main.js
+++ main.js
@@ -1,21 +1,13 @@
 const lastName = "谷口";
-
 const firstName = "義明";
-
 const fullName = lastName + " " + firstName;
 
 const myAge = 26;
-
 const friendAge = 13;
-
 const totalAge = myAge + friendAge;
-
 const numberOfDays =  totalAge * 365;
 
 const numberOfChildren = 3;
-
 const numberOfCake = 10;
-
 const numberOfCakesByChild = numberOfCake / numberOfChildren;
-
 const numberOfRestOfCakes = numberOfCake % numberOfChildren;
