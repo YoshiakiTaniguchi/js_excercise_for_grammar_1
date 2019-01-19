98f39b75fa509cc28312815cd55af085fbcf85e9
YoshiakiTaniguchi <tanitani717@outlook.com>
2019-01-19 16:14:49 +0900

    second commit

--- main.js
+++ main.js
@@ -11,3 +11,18 @@ const numberOfChildren = 3;
 const numberOfCake = 10;
 const numberOfCakesByChild = numberOfCake / numberOfChildren;
 const numberOfRestOfCakes = numberOfCake % numberOfChildren;
+
+console.log(lastName)
+console.log(firstName)
+console.log(fullName)
+console.log(fullName)
+
+console.log(myAge)
+console.log(friendAge)
+console.log(totalAge)
+console.log(numberOfDays)
+
+console.log(numberOfChildren)
+console.log(numberOfCake)
+console.log(numberOfCakesByChild)
+console.log(numberOfRestOfCakes)
